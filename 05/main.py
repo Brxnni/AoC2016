74704cd1b59a63fa7ab568dcb009998e44703109
BuildTools <unconfigured@null.spigotmc.org>
2021-12-29 17:00:53 +0100

    add day 5 and playsound file

---
+++ 05/main.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import hashlib
+
+fullPath = "\\".join(os.path.realpath(__file__).split("\\")[:-2])
+sys.path.append(fullPath)
+import playsound
+
+def part1(string: str) -> str:
+	password = ""
+	i = 0
+	while True:
+		# Hash and turn to hex
+		hexHash = hashlib.md5((string + str(i)).encode()).hexdigest()
+		if hexHash.startswith("00000"):
+			# Add 6th character
+			password += hexHash[5]
+			# Return when done
+			if len(password) == 8: return password
+		i += 1
+
+def part2(string: str) -> str:
+	password = ["_" for _ in range(8)]
+	i = 0
+	while True:
+		# Hash and turn to hex
+		hexHash = hashlib.md5((string + str(i)).encode()).hexdigest()
+		if hexHash.startswith("00000"):
+			print(i, hexHash, "".join(password))
+			# Char and position are first two non-zero hex digits
+			char, position = hexHash[6], int(hexHash[5], 16)
+			# Only set when position within range and char not already set
+			if position < 8:
+				if password[position] == "_":
+					password[position] = char
+					print("Set char", char, "at position", position, "pw is", "".join(password))
+					# Play sound when one char is complete (my pc is slow sadge)
+					playsound.play(f"{fullPath}\\done.mp3")
+					if password.count("_") == 0: return "".join(password)
+		i += 1
+
+sample = "abc"
+input = "ffykfhsq"
+
+print(part2(input))
--- README.md
+++ README.md
@@ -8,4 +8,19 @@ Advent of Code 2016. There's not much more to know.
   * `puzzle.md`, which contains the puzzle description.
   * `sample.txt`, which contains the sample input.
   * `input.txt`, which contains the whole puzzle input.
-  * (if the .txt files are not included, that means the input is just a single string, like in day 5)
+  * (if the `.txt` files are not included, that means the input is just a single string, like in day 5.)
+* There is also the `playsound.py` file, which can play sounds (the sound can be found [here](https://www.youtube.com/watch?v=DfUvUEUPNnc)), because the computation of day 5 took like half an hour and I didn't want to miss it.
+  You can use
+
+  ```python
+  import os
+  import sys
+  # I can't figure out how to import files from the same directory, so this is the solution.
+  fullPath = "\\".join(os.path.realpath(__file__).split("\\")[:-2])
+  sys.path.append(fullPath)
+  import playsound
+  # Actually play the sound after 5 lines
+  playsound.play(f"{fullPath}\\done.mp3")
+  ```
+
+  to play the sound.
