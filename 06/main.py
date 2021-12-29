a4be098c0e8e5b28d101ca43ae1667b22cd787bc
BuildTools <unconfigured@null.spigotmc.org>
2021-12-29 17:13:09 +0100

    Add day 6

---
+++ 06/main.py
@@ -0,0 +1,34 @@
+import os
+
+def read() -> str:
+	path = "\\".join(os.path.realpath(__file__).split("\\")[:-1])
+	with open(f"{path}\\input.txt", "r") as file:
+		return file.read()
+
+def part1(file: str) -> str:
+	transmissions = file.split("\n")
+	length = len(transmissions[0])
+	message = ""
+	for i in range(length):
+		allChars = "".join([transmission[i] for transmission in transmissions])
+		charCounts = [[char, allChars.count(char)] for char in set(allChars)]
+		charCounts.sort(key = lambda c: c[1])
+		charCounts.reverse()
+		mostCommonChar = charCounts[0][0]
+		message += mostCommonChar
+	return message
+
+# This is just part 1 but checking for the least common char (i.e. not reversing the charCounts list)
+def part2(file: str) -> str:
+	transmissions = file.split("\n")
+	length = len(transmissions[0])
+	message = ""
+	for i in range(length):
+		allChars = "".join([transmission[i] for transmission in transmissions])
+		charCounts = [[char, allChars.count(char)] for char in set(allChars)]
+		charCounts.sort(key = lambda c: c[1])
+		mostCommonChar = charCounts[0][0]
+		message += mostCommonChar
+	return message
+
+print(part2(read()))
